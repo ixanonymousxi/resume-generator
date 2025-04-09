3b24d0da60aaaf16107c7223ebf34b93a843f791
Sarah <louise562@aol.com>
2025-04-08 21:08:49 -0500

    updating config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/resume-generator/'
+  base: '/resume-generator/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
